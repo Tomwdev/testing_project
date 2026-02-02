2de91f0e24836c7386059ffe369d80a4ae329c7d
tomwdev <14twoody14@gmail.com>
2026-02-02 18:37:32 +1000

    adjust tag seeder to include slug instead of description

--- database/migrations/2026_01_29_113008_create_tags_table.php
+++ database/migrations/2026_01_29_113008_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
